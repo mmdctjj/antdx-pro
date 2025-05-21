5efc17a0933881ecfe4ba7e1113f660a976f8036
mmdctjj <984808285@qq.com>
2025-05-21 15:29:20 +0800

    feat: 修改logo

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'dumi';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   outputPath: 'docs-dist',
   base: '/antdx-pro/',
+  favicons: [isProd ? '/antdx-pro/favicon.ico' : '/favicon.ico'],
+  publicPath: isProd ? '/antdx-pro/' : '/',
   themeConfig: {
-    name: '@antdx/pro',
+    name: 'antdx-pro',
+    logo: isProd ? '/raetantdx-proable/logo.png' : '/logo.png',
+    prefersColor: { default: 'auto' },
+    footer: '@antdx/pro',
+    socialLinks: {
+      github: 'https://github.com/mmdctjj/antdx-pro',
+    },
   },
 });
